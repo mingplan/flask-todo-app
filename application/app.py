56c5ba433b73d2619c94cb64186c8d45647a19d5
hmzhu <hmzhu@freewheel.tv>
2018-03-09 09:03:14 +0800

    add code

--- application/app.py
+++ application/app.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask_restful import Api
 from controllers import blueprints
 from configs import config
 from extensions import db
+from application.controllers.api import Todo, TodoList
 
 
 def create_app(config_name=None):
@@ -19,5 +21,12 @@ def create_app(config_name=None):
     # blueprints
     for bp in blueprints:
         app.register_blueprint(bp)
+
+    # flask restful plugin
+
+    api = Api(app)
+
+    api.add_resource(TodoList, '/todos')
+    api.add_resource(Todo, '/todos/<string:todo_id>')
 
     return app
